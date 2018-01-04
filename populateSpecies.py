1535cb2a37fcce321e7728abdf88846e44b291d0
caitlynlee <caitlyn.19@dartmouth.edu>
2018-01-04 15:28:24 -0500

    Signed

--- populateSpecies.py
+++ populateSpecies.py
@@ -6,7 +6,7 @@ cwd = os.getcwd()
 
 complete = "testComplete.csv"
 empty = "testIncomplete.csv"
-filled = "testIncompleteFILLED.csv"
+filled = empty.split(".")[0] + "ALLTHEWAYDONE.csv"
 
 idToSpecies = os.path.join(cwd, complete)
 toBeFilled = os.path.join(cwd, empty)
@@ -23,19 +23,23 @@ with open(idToSpecies, 'rU') as completeFile:
 
     for row in reader:
         #str.rstrip(row[0])
+        # 0 should be replaced by column of ID# - 1
+        # 1 should be repaced by column of species name - 1
         idDict[row[0]] = row[1]
 
-print idDict
 with open(toBeFilled) as emptyFile:
     reader = csv.reader(emptyFile)
 
     # if header, uncomment
-    #header = next(reader)
+    header = next(reader)
 
     for row in reader:
+        # again 0 is the column # of ID# -1
         if row[0] not in idDict:
             print "id " + str(row[0]) + " not found in complete spreadsheet"
         else:
+            # 4 is the column in the unfilled spreadhsheet where the name
+            # should go ( again subtract 1)
             row[4] = idDict[row[0]]
         data.append(row)
 
